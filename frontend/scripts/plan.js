e80ae1e7f2b28b79b30caaaa3f81922eed4941ac
iscgrnt <4xth4xth@gmail.com>
2021-05-07 12:05:03 +0200

    7-5-21_inicio

---
+++ frontend/scripts/plan.js
@@ -0,0 +1,25 @@
+window.onload = actions;
+window.onresize = actions2;
+
+function actions() {
+
+    let alto = document.querySelector("#logo").offsetHeight;
+    document.querySelector("header").style.height = alto + "px";
+    document.querySelector("#moreDropdown").style.top = alto + "px";
+    actions2();
+}
+function actions2() {
+
+    document.querySelector("main").style.height = window.innerHeight - Number(document.querySelector("header").offsetHeight) - Number(document.querySelector("nav").offsetHeight) + "px";
+
+    document.querySelectorAll(".planMedia")[0].style.height = window.innerHeight - Number(document.querySelector("header").offsetHeight) - Number(document.querySelector("nav").offsetHeight) - Number(document.querySelector("#information").offsetHeight) + "px";
+}
+
+function actions3() {
+    var x = document.querySelector("#monsta");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
---
+++ frontend/scripts/script.js
@@ -0,0 +1,50 @@
+window.onload = actions;
+window.onresize = actions2;
+
+function actions() {
+
+    let alto = document.querySelector("header").offsetHeight;
+    document.querySelector("#moreDropdown").style.top = alto + "px";
+    actions2();
+
+    document.querySelectorAll(".calDay")[3].onscroll = myFunction;
+}
+function actions2() {
+    document.querySelector("main").style.height = window.innerHeight - 48 - 41 + "px";
+}
+
+function actions3() {
+    var x = document.querySelectorAll(".planContent")[0];
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function actions4() {
+    var x = document.querySelectorAll(".planContent")[1];
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function actions5() {
+    var x = document.querySelector("#burgerDropdown");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction() {
+  if (document.querySelectorAll(".calDay")[3].scrollTop > 0) {
+    document.querySelectorAll(".days")[3].className = "slideUp";
+    
+}else{
+    document.querySelectorAll(".slideUp")[0].className = "days";
+}
+}
