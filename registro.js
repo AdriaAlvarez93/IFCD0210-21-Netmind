928d167f3208b0e87c2f56d78ece2e661ea13512
Armand Laplana <armando.lap@gmail.com>
2021-06-15 10:47:25 +0200

    15/6/21

---
+++ registro.js
@@ -0,0 +1,11 @@
+
+const bnt = document.getElementsByClassName("btn")
+const iniciarSesion = document.getElementsByClassName("IniciarSesion")
+const registrarse = document.getElementsByClassName("Registrarse")
+
+alert("Hola")
+
+
+
+//----------------------Validación--------------------------
+
